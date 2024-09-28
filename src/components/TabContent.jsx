6687611c5ecacac367c3e4052f5bb0116c868c99
eduard165 <112569276+eduard165@users.noreply.github.com>
2024-09-28 01:52:56 -0600

    &#39; error '

--- src/components/TabContent.jsx
+++ src/components/TabContent.jsx
@@ -27,7 +27,7 @@ export default function TabsContentComponent() {
           <div>
             <div>
               <h3 className="text-xl font-bold mb-2">About me</h3>
-              <p>I'm Eduardo, a passionate developer.</p>
+              <p>I am Eduardo, a passionate developer.</p>
               <p>
                 I am a final-year Computer Technology undergraduate with a strong interest in software development. I have hands-on experience with languages such as Java, JavaScript, and SQL. I enjoy learning and exploring new technologies, and I have built simple APIs as part of my learning journey.
               </p>
